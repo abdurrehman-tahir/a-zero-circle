e5be80da1869ba92b6d062cfced792a4bfe56dd1
Abdur R. Tahir <abdurrehmanbintahir@gmail.com>
2026-05-19 02:51:34 +0500

    aho

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-import type { Config } from 'tailwindcss';
-
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
@@ -22,4 +21,4 @@ export default {
     }
   },
   plugins: []
-} satisfies Config;
+}
